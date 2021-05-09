9ca6961bbeff2bfe07e7b7e53258c2cf233e325f
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-05-09 21:43:12 +1000

    Task 1 Complete

--- BuildQueryChallenge.sql
+++ BuildQueryChallenge.sql
@@ -1,1 +1,21 @@
 --Stavros Kyriacou 101610510
+
+--RELATIONAL SCHEMA
+
+--Subject (SubjectCode, Description)
+--PRIMARY KEY (SubjCode)
+
+--Teacher (StaffID, Surname, GivenName)
+--PRIMARY KEY (StaffID)
+
+--SubjectOffering (Year, Semester, Fee, SubjCode)
+--PRIMARY KEY (Year, Semester, Subjcode)
+--FOREIGN KEY (SubjCode) REFERENCES Subject
+
+--Student (StudentID, Surname, GIvenName, Gender)
+--PRIMARY KEY (StudentID)
+
+--Enrolment (StudentID, Year, Semester)
+--PRIMARY KEY (StudenID, Year, Semester)
+--FOREIGN KEY (StudentID) REFERENCES Student
+--FOREIGN KEY (Year, Semester) REFERENCES SubjectOffering
